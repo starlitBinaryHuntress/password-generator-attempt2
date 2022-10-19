8e0337331e5a1fc2517e1ff26804bf9b0cce2db2
Jordan <jnbarringer96@gmail.com>
2022-10-18 19:12:48 -0500

    password now generates but is not the length the user inputs

--- assets/script.js
+++ assets/script.js
@@ -13,20 +13,6 @@ var numbersEl = document.querySelector("#numbers");
 
 var specialEl = document.querySelector("#special");
 
-//create window prompts
-var lowercasePrompt = prompt("Would you like your generated password to contain lowercase letters?")
-var uppercasePrompt = prompt("Would you like your generated password to contain uppercase letters?")
-var numbersPrompt = prompt("Would you like your generated password to contain numbers?")
-var specialPrompt = prompt("Would you like your generated password to contain special characters?")
-var lengthPrompt = prompt("How long would you like your password to be? Answer must be between 8-128 characters")
-
-const variables = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
-}
-
 function getRandomLower() {
   return String.fromCharCode (Math.floor(Math.random() * 26 + 97));
 }
@@ -44,43 +30,41 @@ function getRandomSymbol() {
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
-function generatePassword(lowercasePrompt, uppercasePrompt, numbersPrompt, specialPrompt, lengthPrompt) {
+function generatePassword() {
   var generatedPassword = '';
+  var pw = ""
 
-  if (lowercasePrompt.toLowerCase() === "yes") {
+  //create window prompts
+  var lowercasePrompt = confirm("Would you like your generated password to contain lowercase letters?")
+  var uppercasePrompt = confirm("Would you like your generated password to contain uppercase letters?")
+  var numbersPrompt = confirm("Would you like your generated password to contain numbers?")
+  var specialPrompt = confirm("Would you like your generated password to contain special characters?")
+  var lengthPrompt = prompt("How long would you like your password to be? Answer must be between 8-128 characters")
+  lengthPrompt = parseInt(lengthPrompt)
+  console.log(lengthPrompt)
+
+  if (lowercasePrompt) {
     generatedPassword += getRandomLower();
   }
-  if (uppercasePrompt.toLowerCase() === "yes") {
+  if (uppercasePrompt) {
     generatedPassword += getRandomUpper();
   }
-  if (numbersPrompt.toLowerCase() === "yes") {
+  if (numbersPrompt) {
     generatedPassword += getRandomNumber();
   }
-  if (specialPrompt.toLowerCase() === "yes") {
+  if (specialPrompt) {
     generatedPassword += getRandomSymbol();
   }
 
-  var typesCount = lower + upper + number + symbol;
-
-  if(typesCount === 0) {
-    return "Please refresh the page and enter your criteria."
-  }
-
   if(lengthPrompt < 8 || lengthPrompt > 128) {
-    return "The password must be more than 8 characters and less than 128."
+    alert("The password must be more than 8 characters and less than 128.")
+    return ""
   }
 
-  for (let i = 0; i < lengthPrompt; i += typesCount) {
-    typesArr.forEach(type => {
-      const funcName = Object.keys(type)[0];
-      console.log('funcName: ', funcName)
-
-      generatedPassword += variables[funcName]();
-    });
-  }
   var finalPassword = generatedPassword.slice(0, 128);
-  return finalPassword;
+  finalPassword.length = lengthPrompt
+  passwordText.value = finalPassword;
 }
 
 // Add event listener to generate button and submit button
-generateBtn.addEventListener("click", generatePassword());
+generateBtn.addEventListener("click", generatePassword);
